293c8cf3bdffea911fc50ffc5646b3c8fc9f8688
mfernandez <mfernandezfie@gmail.com>
2026-02-22 14:08:22 +0100

    feat/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Lato } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const playfair = Playfair_Display({
@@ -38,6 +39,7 @@ export default function RootLayout({
         className={`${playfair.variable} ${lato.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
